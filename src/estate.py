2b4aae8ba05185a10cf5e7974695ddd6a647eca8
slpy22 <112932550+slpy22@users.noreply.github.com>
2024-07-05 16:31:18 +0900

    일단 작동은 되는데 결과는 형편 없음

--- src/estate.py
+++ src/estate.py
@@ -1,14 +1,37 @@
-import gc
-import time
-import numpy as np
-import pandas as pd
-from ..common.log_config import log
+import os
+from pathlib import Path
+import base64
+from flask import jsonify
+from mimetypes import guess_type
+from my_langchain import extract_information, recommend_estate
+
+
+# Function to encode a local image into data URL
+def image_to_data_url(image_file):
+    mime_type, _ = guess_type(image_file.filename)
+    # Default to png
+    if mime_type is None:
+        mime_type = 'image/png'
+
+    base64_encoded_data = base64.b64encode(image_file.read()).decode('utf-8')
+
+    # Construct the data URL
+    return f"data:{mime_type};base64,{base64_encoded_data}"
+
 
 def predict(img_file):
     """
     이미지 분석하여 후보 추천 하는 함수
     :return: 매물 위치 도로명 주소의 배열
     """
-    ret = [(0 if sdid not in limit_table.keys() or item < limit_table[sdid] else 1) for item in ret]
-    ret = str(ret)
-    return ret, 200
+    # file_path = os.path.join(os.getcwd(), img_file.filename)
+    # img_file.save(file_path)
+
+    img_data = image_to_data_url(img_file)
+
+    information = extract_information(img_data)
+    addresses = recommend_estate(information)
+    # addresses = ["123 Main St, Springfield, IL", "456 Elm St, Shelbyville, IL"]  # 예제 응답
+
+    # os.remove(file_path)
+    return jsonify(addresses), 200
